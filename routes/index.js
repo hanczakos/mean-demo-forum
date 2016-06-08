51c481e58e163742ddb52a911a08253dfcd23a04
Ákos Hancz <akos.hancz@blackswan.com>
2016-06-08 17:32:11 +0200

    fixed a route

--- routes/index.js
+++ routes/index.js
@@ -83,7 +83,7 @@ router.post('/posts/:post/comments', function(req, res, next) {
   });
 });
 
-router.put('/post/:post/comments/:comment/upvote', function(req, res, next) {
+router.put('/posts/:post/comments/:comment/upvote', function(req, res, next) {
   req.comment.upvote(function(err, comment){
     if (err) { return next(err); }
 
